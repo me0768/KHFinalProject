924caf72ef05599d54b7ea18e0b820cfb7f6921a
junilpark <jipark1106@gmail.com>
2017-11-27 20:46:28 +0900

    Revert "Revert "dd""
    This reverts commit 30915ef5719698785ebdd5d9f2bbd2deb28f5de3.

---
+++ FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/user/model/dao/UserDao.java
@@ -0,0 +1,44 @@
+package com.kh.fitnessground.user.model.dao;
+
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.fitnessground.user.model.vo.User;
+
+@Repository("userDao")
+public class UserDao {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	public UserDao() {}
+
+	public User logincheck(String email) {
+		User user = sqlSession.selectOne("user.selectUser", email);
+		return user;
+	}
+
+	public User list(int user_no) {
+		return sqlSession.selectOne("user.selectInfo", user_no);
+	}
+
+	public User emailcheck(String vo) {
+		return sqlSession.selectOne("user.emailCheck", vo);
+	}
+
+	public User nicknameCheck(String nickname) {
+		return sqlSession.selectOne("user.nicknameCheck", nickname);
+	}
+
+	public void registerCheck(User user) {
+		sqlSession.insert("user.register", user);
+	}
+
+	public User findid(User user) {
+		User u = sqlSession.selectOne("user.findid", user);
+		System.out.println(u);
+		return u;
+	}
+}
---
+++ FinalWorkspace/FitnessGround/src/main/webapp/WEB-INF/views/health/healthMain.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri= "http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+ <c:import url="./include/common/head.jsp" />
+
+  <style type="text/css">
+
+  </style>
+
+ <c:import url="./include/common/headend.jsp" />
+	
+	<body>
+	
+	</body>
+	
+<c:import url="./include/common/end.jsp" />
+</html>

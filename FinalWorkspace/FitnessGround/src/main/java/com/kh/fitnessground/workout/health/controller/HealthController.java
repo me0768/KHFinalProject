ba05091569d1aee28b9b3a5f2fd799f3473fd9f7
junilpark <jipark1106@gmail.com>
2017-11-29 15:21:00 +0900

    헬스 부분 컨트롤러 작업중

--- FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/workout/health/controller/HealthController.java
+++ FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/workout/health/controller/HealthController.java
@@ -1,7 +1,12 @@
 package com.kh.fitnessground.workout.health.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.fitnessground.workout.health.model.service.HealthService;
 
@@ -10,4 +15,19 @@ public class HealthController {
 	@Autowired
 	private HealthService healthService;
 	
+	@RequestMapping(value="healthMain.do")
+	public ModelAndView boardListMethod(HttpServletRequest request, HttpServletResponse response){
+		
+		ModelAndView mv = new ModelAndView("workout/healthMain");
+		
+		return mv;
+	}
+	
+	
+	
+	
+	@RequestMapping(value="aroundFitenessCenter.do")
+	public String aroundFitnessMethod(){
+		return "";
+	}
 }
--- FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/workout/health/model/service/HealthServiceImpl.java
+++ FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/workout/health/model/service/HealthServiceImpl.java
@@ -1,12 +1,27 @@
 package com.kh.fitnessground.workout.health.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.fitnessground.workout.health.model.dao.HealthDao;
+import com.kh.fitnessground.workout.health.model.vo.Health;
 
 @Service("healthService")
 public class HealthServiceImpl implements HealthService{
 	@Autowired
 	HealthDao healthDao;
+
+	@Override
+	public ArrayList<Health> getList() {
+		// TODO Auto-generated method stub
+		return healthDao.getList();
+	}
+
+	@Override
+	public Health selectWorkout(int vno) {
+		// TODO Auto-generated method stub
+		return healthDao.selectOneWorkout(vno);
+	}
 }

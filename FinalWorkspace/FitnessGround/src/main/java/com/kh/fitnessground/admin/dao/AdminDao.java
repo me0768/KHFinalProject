00f1040f3bf6ad5c3a5a1e78b67dcccde78e3203
dlrbdus02 <dlrbdus02@nate.com>
2017-12-02 15:57:59 +0900

    관리자클래스메소드
    관리자클래스메소드

--- FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/admin/dao/AdminDao.java
+++ FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/admin/dao/AdminDao.java
@@ -1,5 +1,22 @@
 package com.kh.fitnessground.admin.dao;
 
-public class AdminDao {
+import java.util.ArrayList;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.fitnessground.user.model.vo.User;
+
+@Repository("AdminDao")
+public class AdminDao {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	public ArrayList<User> userlist;
+	
+	public ArrayList<User> businesslist;
+	
+	
+	
 }
--- FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/admin/service/AdminServiceImpl.java
+++ FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/admin/service/AdminServiceImpl.java
@@ -1,6 +1,28 @@
 package com.kh.fitnessground.admin.service;
 
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.fitnessground.admin.dao.AdminDao;
+import com.kh.fitnessground.user.model.dao.UserDao;
+import com.kh.fitnessground.user.model.vo.User;
+
+@Service("AdminService")
 public class AdminServiceImpl  implements AdminService{
 
+	@Autowired
+	AdminDao AdminDao;
+	
+
+	public ArrayList<User> userlist(int userno, int userlevel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<User> buisnesslist(int userno, int userlevel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

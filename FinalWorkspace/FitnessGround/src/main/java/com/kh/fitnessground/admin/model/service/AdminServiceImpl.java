660300e57e9c8b93ddc94bfd81f713292d173274
ydk0831 <ydk0831@gmail.com>
2017-12-04 10:02:02 +0900

    Revert "Revert "ㅇ""
    This reverts commit 357122b2efb7d34b59e5318788a144c86ccb6652, reversing
    changes made to 2d3db8da765482f8301adbdc28710a11c1b48074.

--- FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/admin/model/service/AdminServiceImpl.java
+++ FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/admin/model/service/AdminServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.fitnessground.admin.model.dao.AdminDao;
-import com.kh.fitnessground.user.model.dao.UserDao;
 import com.kh.fitnessground.user.model.vo.User;
 
 @Service("adminService")
--- FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/community/controller/CommunityBoardController.java
+++ FinalWorkspace/FitnessGround/src/main/java/com/kh/fitnessground/community/controller/CommunityBoardController.java
@@ -1,25 +1,27 @@
 package com.kh.fitnessground.community.controller;
 
-import java.util.Locale;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.kh.fitnessground.community.model.service.CommunityBoardService;
 
 @Controller
 public class CommunityBoardController {
-
+	@Autowired
+	private CommunityBoardService communityBoardService;
+	
 	//리스트
-		@RequestMapping(value = "community.do", method = RequestMethod.GET)
-		public String home(Locale locale, Model model) {
+		@RequestMapping(value = "community.do")
+		public String home() {
 			return "community/communityMain";
 		}
 		
 	//운동같이해요 게시판
 		
-				@RequestMapping(value = "meeting.do", method = RequestMethod.GET)
-				public String meetingBoard(Locale locale, Model model) {
+				@RequestMapping(value = "meeting.do")
+				public String meetingBoard() {
+					communityBoardService.meetingView();
 					return "community/meetingBoard/meetingBoard";
 				}
 			
@@ -29,8 +31,8 @@ public class CommunityBoardController {
 	
 	//후기 게시판
 				//리스트
-				@RequestMapping(value = "qna.do", method = RequestMethod.GET)
-				public String qnaBoard(Locale locale, Model model) {
+				@RequestMapping(value = "qna.do")
+				public String qnaBoard() {
 					return "community/qnaBoard/qnaBoard";
 				}
 		
@@ -39,8 +41,8 @@ public class CommunityBoardController {
 		
 	//qna 게시판
 				//리스트
-				@RequestMapping(value = "review.do", method = RequestMethod.GET)
-				public String reviewBoard(Locale locale, Model model) {
+				@RequestMapping(value = "review.do")
+				public String reviewBoard() {
 					return "community/reviewBoard/reviewBoard";
 				}
 		
